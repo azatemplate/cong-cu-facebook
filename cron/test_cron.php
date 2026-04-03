7228074ae69cdda2b88550526fc9fce3ba81188c
azatemplate <azatemplate@gmail.com>
2026-04-03 20:02:37 +0700

    Fix: Stop test_cron.php from downloading by forcing text/html header and adding a redirect to diagnostics.php

---
+++ cron/test_cron.php
@@ -0,0 +1,10 @@
+<?php
+// cron/test_cron.php
+// Đã được chuyển về thư mục gốc để vượt Tường Lửa Nginx.
+header('Content-Type: text/html; charset=utf-8');
+header('Location: ../diagnostics.php');
+exit;
+?>
+<meta http-equiv="refresh" content="0; url=../diagnostics.php">
+<script>window.location.href="../diagnostics.php";</script>
+Đang chuyển hướng sang trang Chẩn đoán mới...
