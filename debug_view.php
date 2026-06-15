f42aa8f02aa7e0c369c097fddc12742f544f7f3a
azatemplate <azatemplate@gmail.com>
2026-06-15 14:50:48 +0700

    cleanup: remove temporary debug_view.php file

--- debug_view.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-$logFile = __DIR__ . '/zalo_webhook_debug.log';
-if (file_exists($logFile)) {
-    echo file_get_contents($logFile);
-} else {
-    echo "Log file not found.";
-}
