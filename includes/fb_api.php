5d9c8fa707c8a0e094adf491bd450d8f6369896d
azatemplate <azatemplate@gmail.com>
2026-09-07 16:30:21 +0700

    Fix DNS resolution timeout: add CURLOPT_DNS_CACHE_TIMEOUT and increase connect timeout to 15s

--- includes/fb_api.php
+++ includes/fb_api.php
@@ -8,7 +8,9 @@ define('FB_API_BASE', 'https://graph.facebook.com/' . FB_API_VERSION . '/');
 function fb_curl_setssl($ch) {
     // Force IPv4 resolution to prevent IPv6 connection timeouts on VPS/servers
     curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+    curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 3600);
+    curl_setopt($ch, CURLOPT_ENCODING, '');
 
     // Disable SSL verification only in development environment
     if (defined('APP_ENV') && APP_ENV === 'development') {
