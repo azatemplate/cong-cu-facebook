83c9f668ebedd2974b6dd5a8a93834083e31b800
azatemplate <azatemplate@gmail.com>
2026-09-15 20:58:59 +0700

    Update default TikTok scopes to user.info.basic,video.upload,user.info.profile matching exact Developer Portal configuration

--- includes/tiktok_api.php
+++ includes/tiktok_api.php
@@ -75,7 +75,7 @@ function get_tiktok_auth_url($redirect_uri, $state = '') {
         return '';
     }
 
-    $scopes = !empty($cfg['scopes']) ? $cfg['scopes'] : 'user.info.basic,video.publish,video.upload,video.list';
+    $scopes = !empty($cfg['scopes']) ? $cfg['scopes'] : 'user.info.basic,video.upload,user.info.profile';
     
     $params = [
         'client_key' => $client_key,
